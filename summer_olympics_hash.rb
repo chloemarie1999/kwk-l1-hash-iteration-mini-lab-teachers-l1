77b9e2939f2fee61878e6607ecb9d66c5b89d111
Chloé P <chloepuck13@yahoo.com>
2018-07-25 18:00:57 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,15 +1,19 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  {Sydney: "2000",
+  Athens: "2004",
+  Beijing: "2008",
+  London: "2012"}
 end
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  new_hash=create_olympics_hash
+  new_hash=[:Atlanta]="1996"
+  new_hash
 end
 
 def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  summer_olympics.each do 
 end
 
 def iterate_through_keys
