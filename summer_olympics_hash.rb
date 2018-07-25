e5f91771a13a2a917aad156d33f533a17d8db182
Chloé P <chloepuck13@yahoo.com>
2018-07-25 18:04:02 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -19,6 +19,6 @@ def iterate_through_hash
 end
 
 def iterate_through_keys
-  upcase_cities=add_a_key_value_pair.map {|key,value|.upcase|
+  upcase_cities=add_a_key_value_pair.map {|key,value.upcase|
   
 end
